02fb54db31868a4c002e8fe71b31af1b550e8843
Aliaksei Komar <a.komar@godeltech.com>
2019-10-09 12:52:06 +0300

    Fixed bugs in App and Task

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import "./App.css";
 import Task from "./Task.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   state = {
     tasks: [
       {
@@ -22,41 +18,50 @@ class App extends React.Component {
   };
 
   taskClicked = event => {
-    let target = event.target || event.srcElement;
     this.setState({
       tasks: this.state.tasks.map(element => {
-        if (element.name === target.innerHTML) {
-          element.isDone === "notDone"
-            ? (element.isDone = "alreadyDone")
-            : (element.isDone = "notDone");
+        if (element.name === event.target.innerHTML) {
+          return {
+            ...element,
+            isDone:
+              element.isDone === "notDone"
+                ? (element.isDone = "alreadyDone")
+                : (element.isDone = "notDone")
+          };
+        } else {
+          return element;
         }
-
-        console.log(element);
-        return element;
       })
     });
   };
+
+  incrementStart = 100;
+  taskValue = "";
 
   taskAdding = event => {
     if (event.keyCode === 13) {
       // Button Enter
       let target = event.target || event.srcElement;
-      this.setState({
-        tasks: this.state.tasks.concat([
-          {
-            name: event.target.value,
-            isDone: "notDone"
-          }
-        ])
+      this.taskValue = event.target.value;
+      this.setState(prevState => {
+        return {
+          tasks: [
+            ...prevState.tasks,
+            {
+              name: this.taskValue,
+              isDone: "notDone",
+              key: this.incrementStart++
+            }
+          ]
+        };
       });
       target.value = "";
     }
   };
 
   searchHandler = event => {
-    let target = event.target || event.srcElement;
     this.setState({
-      searchText: target.value
+      searchText: event.target.value
     });
   };
 
@@ -68,7 +73,7 @@ class App extends React.Component {
           placeholder="Search tasks...."
           className="typesearch"
           onKeyUp={this.searchHandler}
-        ></input>
+        />
         <ul>
           {this.state.tasks
             .filter(
@@ -77,49 +82,23 @@ class App extends React.Component {
                   .toLowerCase()
                   .indexOf(this.state.searchText.toLowerCase()) >= 0
             )
-            .map((task, index) => (
+            .map(task => (
               <Task
                 name={task.name}
                 isDone={task.isDone}
-                key={index}
+                key={task.key}
                 handler={this.taskClicked}
               />
             ))}
         </ul>
         <input
-          placeholder="Add tasks...."
+          placeholder="Add tasks....."
           className="taskAdd"
           onKeyUp={this.taskAdding}
-        ></input>
+        />
       </div>
     );
   }
 }
-
-// function App() {
-//   var
-//   var addingTask = function () {
-//     this.state.tasks.push({
-//       name:
-//     })
-//   }
-//   return (
-//     <div className="wrap">
-//       <h1>
-//         TO-DO List
-//       </h1>
-//       <ul>
-//         {state.tasks.map((task, index) => (
-//           <li className="singeTask" key={index} >
-
-//             {task.name}
-//           </li>
-//         ))}
-//       </ul>
-
-//       <input className="taskAdd" ></input><button onClick={addingTask}>Add task</button>
-//     </div>
-//   );
-// }
 
 export default App;
--- src/Task.js
+++ src/Task.js
@@ -1,20 +1,11 @@
 import React from "react";
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <li
-        onClick={this.props.handler}
-        className={"singeTask " + this.props.isDone}
-      >
-        {this.props.name}
-      </li>
-    );
-  }
-}
+let Task = function(props) {
+  return (
+    <li onClick={props.handler} className={"singeTask " + props.isDone}>
+      {props.name}
+    </li>
+  );
+};
 
 export default Task;
