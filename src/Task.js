46207e2c1bd6edc60abbfdc1c2c1fb3784e64c02
Aliaksei Komar <a.komar@godeltech.com>
2019-10-10 13:05:05 +0300

    Created single CSS for Task file

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Task.css';
 
 export default function Task({handler, status, name}) {
   return (
     <li
       onClick={handler}
-      className={'singeTask ' + (status === 'done' ? 'classDone' : 'classUndone')}
+      className={'singeTask ' + (status === 'done' ? 'viewDone' : 'viewUndone')}
     >
       {name}
     </li>
